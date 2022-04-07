bbff6498608e9f0b57f21a13371f5c325d70b568
Araden <2713886816@qq.com>
2022-04-07 17:30:25 +0800

    登陆界面完成

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,33 @@
 <template>
-  <main-view></main-view>
+  <n-message-provider>
+    <login></login>
+  </n-message-provider>
 </template>
 
 <script>
+import { NMessageProvider } from 'naive-ui'
 import mainView from '@/view/mainView.vue'
+import login from '@/view/login.vue'
 export default {
   setup() {},
   components: {
+    NMessageProvider,
     mainView,
+    login,
   },
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+}
+a {
+  color: #000;
+  text-decoration: none;
+}
+a:hover {
+  color: #18a058;
+}
+</style>
---
+++ src/view/login.vue
@@ -0,0 +1,219 @@
+<template>
+  <div class="container">
+    <n-card class="login" title="欢迎使用实验室设备预约系统！">
+      <n-tabs size="large" animated>
+        <n-tab-pane name="signin" tab="登录">
+          <n-form>
+            <n-input
+              size="large"
+              clearable
+              round
+              class="form-component"
+              placeholder="请输入用户名"
+              v-model:value="user_name"
+            >
+              <template #prefix>
+                <n-icon :component="UserCircle"> </n-icon>
+              </template>
+            </n-input>
+            <n-input
+              size="large"
+              type="password"
+              clearable
+              round
+              show-password-on="click"
+              class="form-component"
+              placeholder="请输入密码"
+              v-model:value="password"
+            >
+              <template #prefix>
+                <n-icon :component="Password"> </n-icon>
+              </template>
+            </n-input>
+          </n-form>
+          <n-button
+            type="primary"
+            size="large"
+            round
+            block
+            secondary
+            strong
+            @click="login"
+          >
+            登录
+          </n-button>
+          <div class="more-operations">
+            <a href="#">忘记密码？</a>
+          </div>
+        </n-tab-pane>
+        <n-tab-pane name="signup" tab="注册">
+          <n-form>
+            <n-input
+              size="large"
+              clearable
+              round
+              class="form-component"
+              placeholder="请输入用户名"
+              v-model:value="user_name"
+            >
+              <template #prefix>
+                <n-icon :component="UserCircle"> </n-icon>
+              </template>
+            </n-input>
+            <n-input
+              size="large"
+              type="password"
+              clearable
+              round
+              show-password-on="click"
+              class="form-component"
+              placeholder="请输入密码"
+              v-model:value="password"
+            >
+              <template #prefix>
+                <n-icon :component="Password"> </n-icon>
+              </template>
+            </n-input>
+            <n-input
+              size="large"
+              clearable
+              round
+              class="form-component"
+              placeholder="请输入要注册的用户类型"
+              v-model:value="user_type"
+            >
+              <template #prefix>
+                <n-icon :component="Types"> </n-icon>
+              </template>
+            </n-input>
+            <n-input
+              size="large"
+              clearable
+              round
+              class="form-component"
+              placeholder="请输入验证码（校外人员不必填写）"
+              v-model:value="user_type"
+            >
+              <template #prefix>
+                <n-icon :component="Verify"> </n-icon>
+              </template>
+            </n-input>
+          </n-form>
+          <n-button
+            type="primary"
+            size="large"
+            round
+            block
+            secondary
+            strong
+            @click="login"
+          >
+            注册
+          </n-button>
+        </n-tab-pane>
+      </n-tabs>
+    </n-card>
+  </div>
+</template>
+
+<script>
+import {
+  NCard,
+  NTabs,
+  NTabPane,
+  NInput,
+  NButton,
+  NForm,
+  NFormItemRow,
+  NIcon,
+  useMessage,
+} from 'naive-ui'
+import { NumberRow24Regular as Verify } from '@vicons/fluent'
+import { UserCircleRegular as UserCircle } from '@vicons/fa'
+import { Password, Types } from '@vicons/carbon'
+import { reactive, toRefs } from 'vue'
+
+export default {
+  setup() {
+    const icons = { UserCircle, Password, Types, Verify }
+    const message = useMessage()
+    const events = reactive({
+      user_name: '',
+      password: '',
+      user_type: '',
+      verify_code: '',
+    })
+    let msgReactive = null
+    function createMessage(type = 'success', info = '', duration = 3000) {
+      msgReactive = message.create(info, {
+        type: type,
+        duration: duration,
+      })
+    }
+    async function login() {
+      if (!events.user_name) {
+        createMessage('warning', '请输入用户名')
+        return
+      } else if (!events.password) {
+        createMessage('warning', '请输入密码')
+        return
+      }
+      createMessage('loading', '登陆中...')
+      let p = new Promise((resolve) =>
+        setTimeout(resolve, 1000, events.user_name)
+      )
+      let userName = await p
+      msgReactive.content = `登陆成功！欢迎，${userName}`
+      msgReactive.type = 'success'
+    }
+    return {
+      ...icons,
+      ...toRefs(events),
+      login,
+    }
+  },
+  components: {
+    NCard,
+    NTabs,
+    NTabPane,
+    NInput,
+    NButton,
+    NForm,
+    NFormItemRow,
+    NIcon,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  position: relative;
+  height: 100vh;
+  width: 100vw;
+  background: center / cover
+    url('https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fi0.hdslb.com%2Fbfs%2Farticle%2Fb579764dfeb722743dd366cd66af9a3956dfffd4.jpg&refer=http%3A%2F%2Fi0.hdslb.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1651903096&t=8f0033ffbe9c9f2f2f195420b804904a')
+    no-repeat;
+  .login {
+    position: absolute;
+    height: 68.6vh;
+    width: 29.2vw;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    border-radius: 3vh;
+    background-color: rgba(255, 255, 255, 0.92);
+    .form-component {
+      background-color: transparent;
+      width: 92%;
+      margin: 1.2vh 4%;
+    }
+    .n-button {
+      width: 92%;
+      margin: 1.2vh 4%;
+    }
+    .more-operations {
+      margin: 2vh 0;
+    }
+  }
+}
+</style>
