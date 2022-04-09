0373258d5184c8216fc80510b60b2c2c476b6455
ERUIHNIYHBKBNF <839203521@qq.com>
2022-04-09 23:42:36 +0800

    approval

--- src/views/approval/approval.vue
+++ src/views/approval/approval.vue
@@ -1,13 +1,14 @@
 <template>
   <NCard title="预约审批" style="width: 98%; margin: 10px 1%; min-height: 85%;">
-    <NDataTable :columns="columns" />
+    <NDataTable :columns="columns" :data="approvals" />
   </NCard>
 </template>
 <script>
-import { NCard, NDataTable, NButton, useDialog } from 'naive-ui';
-import { App } from '@vicons/carbon';
+import { NCard, NDataTable, NButton, useDialog, useMessage } from 'naive-ui';
+import { getApproval, setApproval } from '@/api/approval';
+import ApprovalDetails from './components/ApprovalDetail';
 
-const columns = [
+const getColumns = ({showApprovalDetails, acceptApproval, rejectApproval}) => [
   {
     title: "设备名称",
   },
@@ -21,8 +22,24 @@ const columns = [
     title: "预约人",
   },
   {
+    title: "状态",
+    filterOptions: [
+      {
+        label: '已审批',
+        value: 'London',
+      },
+      {
+        label: '未审批',
+        value: 'New York',
+      },
+    ],
+    filter (value, row) {
+      return 1;
+    }
+  },
+  {
     title: "操作",
-    render(row) {
+    render: (row) => {
       return h('div', {}, [
         h(NButton, {
           type: "info",
@@ -30,27 +47,57 @@ const columns = [
         }, { default: "详情" }),
         h(NButton, {
           type: "success",
-          onClick: () => acceptApproval(row),
+          onClick: () => acceptApproval(row.ReservationID),
         }, { default: "通过" }),
         h(NButton, {
           type: "error",
-          onClick: () => rejectApproval(row),
+          onClick: () => rejectApproval(row.ReservationID),
         }, { default: "拒绝" }),
       ]);
     }
   },
 ];
-function showApprovalDetails(row) {
-  const dialogOptions = {
-  title: "预约申请详情",
-  content: () => h(ApprovalDetails, {
-    info: row,
-  }),
-};
-}
 export default {
   setup() {
     const dialog = useDialog();
+    const message = useMessage();
+    const approvals = ref([]);
+    const showApprovalDetails = (row) => {
+      const dialogOptions = {
+        title: "预约申请详情",
+        content: () => h(ApprovalDetails, {
+          info: row,
+        }),
+      };
+      dialog.show(dialogOptions);
+    };
+    const getAllApproval = async () => {
+      getApproval(1).then(res => {
+        approvals.value = res.Data.Approval;
+      });
+    };
+    const acceptApproval = (id) => {
+      setApproval(id, 1).then(() => {
+        message.success("操作成功");
+        getAllApproval();
+      });
+    };
+    const rejectApproval = (id) => {
+      setApproval(id, 2).then(() => {
+        message.success("操作成功");
+        getAllApproval();
+      });
+    };
+    const columns = getColumns({
+      showApprovalDetails,
+      acceptApproval,
+      rejectApproval,
+    });
+    return {
+      approvals,
+      columns,
+      dialog,
+    };
   },
   components: {
     NCard,
